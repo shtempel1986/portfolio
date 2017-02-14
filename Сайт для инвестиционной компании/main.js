0d9c42d9bf83317a6128880b8e1521590ee9f156
Pavel Ledovskikh <o_gredi@mail.ru>
2017-02-14 19:09:06 +0300

    Р·Р°РєРѕРЅС‡РёР» РІРµСЂСЃС‚РєСѓ СЃР°Р№С‚Р° РґР»СЏ РёРЅРІРµСЃС‚РёС†РёРѕРЅРЅРѕР№ РєРѕРјРїР°РЅРёРё

---
+++ Сайт для инвестиционной компании/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Павел on 14.02.2017.
+ */
+$(function () {
+    // инициализировать все элементы на страницы, имеющих атрибут data-toggle="tooltip", как компоненты tooltip
+    $('[data-toggle="tooltip"]').tooltip()
+});
