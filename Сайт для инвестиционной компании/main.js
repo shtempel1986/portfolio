c1fe7304cd1545930b042c15a2c9259b318e61cf
Pavel Ledovskikh <o_gredi@mail.ru>
2017-02-15 15:43:12 +0300

    СЃРґРµР»Р°Р» РґРІР° СЃР»Р°Р№РґРµСЂР° РїСЂРё РїРѕРјРѕС‰Рё bxslider

--- Сайт для инвестиционной компании/main.js
+++ Сайт для инвестиционной компании/main.js
@@ -3,5 +3,31 @@
  */
 $(function () {
     // инициализировать все элементы на страницы, имеющих атрибут data-toggle="tooltip", как компоненты tooltip
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+    //Слайдер для новостей______________________________________________________________________________________
+    $('#news__slider').bxSlider({
+        nextSelector: '#news__slider-next',
+        prevSelector: '#news__slider-prev',
+        nextText: '<i class="fa fa-chevron-right"></i>',
+        prevText:'<i class="fa fa-chevron-left"></i>',
+        minSlides: 2,
+        maxSlides: 2,
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        pager: false,
+        slideWidth: 480
+    });
+    //Слайдер для отзывов________________________________________________________________________________________
+    $('#testimonials__slider').bxSlider({
+        nextSelector: '#testimonials__slider-next',
+        prevSelector: '#testimonials__slider-prev',
+        nextText: '<i class="fa fa-chevron-right"></i>',
+        prevText:'<i class="fa fa-chevron-left"></i>',
+        minSlides: 2,
+        maxSlides: 2,
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        pager: false,
+        slideWidth: 480
+    });
 });
